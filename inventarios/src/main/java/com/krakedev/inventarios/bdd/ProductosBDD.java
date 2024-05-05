5e0097cde1b3420830eb3f7c736922791d5801b7
AlvaroH20 <alvarodorian@outlook.es>
2024-05-05 16:24:11 -0500

    Reto 11 completo

--- inventarios/src/main/java/com/krakedev/inventarios/bdd/ProductosBDD.java
+++ inventarios/src/main/java/com/krakedev/inventarios/bdd/ProductosBDD.java
@@ -75,4 +75,34 @@ public class ProductosBDD {
 		}
     	return productos;
     }
+	public void insertar(Producto producto) throws KrakeDevException{
+		 Connection con=null;
+		 PreparedStatement ps=null;
+		 try {
+	            con = ConexionBDD.obtenerConexion();
+	            ps = con.prepareStatement(" insert into productos (codigo_producto,nombre,udm,precio_venta,tiene_iva,coste,categoria,stock) "
+	            		+ " values(?,?,?,?,?,?,?,?) ");
+	            ps.setInt(1, producto.getCodigo());
+	            ps.setString(2, producto.getNombre());
+	            ps.setString(3, producto.getUnidadMedida().getNombre());
+	            ps.setBigDecimal(4, producto.getPrecioVenta());
+	            ps.setBoolean(5, producto.isTieneIva());
+	            ps.setBigDecimal(6, producto.getCoste());
+	            ps.setInt(7, producto.getCategoria().getCodigo());
+	            ps.setInt(8, producto.getStock());
+	            
+	            ps.executeUpdate();
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	            throw new KrakeDevException("Error al insertar el cliente. Detalle: "+e.getMessage());
+	        } finally {
+	            if (con != null) {
+	                try {
+	                    con.close();
+	                } catch (SQLException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        }
+	 }
 }
